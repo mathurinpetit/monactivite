1cce88fde081e25c33be0f3084ff213ea154dfbd
Vincent LAURENT <vince.laurent@gmail.com>
2015-06-07 02:17:34 +0200

    Mail importer

---
+++ src/AppBundle/Importer/Mail/MailImporter.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace AppBundle\Importer\Mail;
+
+use Symfony\Component\Console\Output\OutputInterface;
+use AppBundle\Importer\Importer;
+use Html2Text\Html2Text;
+
+class MailImporter extends Importer
+{
+    protected $mailParser;
+
+    public function __construct($am, $em, $mailParser)
+    {
+        parent::__construct($am, $em);
+
+        $this->mailParser = $mailParser;
+    }
+
+    public function run($argument, OutputInterface $output, $dryrun = false) {
+        $mail = null;
+        $start = false;
+        $handle = fopen($argument, "r");
+
+        while (($line = fgets($handle)) !== false) {
+            if(preg_match('/^(From .?$|From - )/', $line)) {
+                if($mail && $start) {
+                    $this->importMail($mail, $output, $dryrun);
+                }
+                $mail = null;
+                $start = true;
+                continue;
+            }
+
+            $mail .= $line;
+        }
+
+        if($mail && $start) {
+            $this->importMail($mail, $output, $dryrun);
+        }
+
+        fclose($handle);
+    }
+
+    protected function importMail($mail, OutputInterface $output, $dryrun = false) {
+        try {
+            $parsedMail = @$this->mailParser->parse($mail);
+        } catch(\Exception $e) {
+            if($output->isVerbose()) {
+                $output->writeln("<error>Error ".$e->getMessage()."</error>");
+            }
+            
+            return false;
+        }
+
+        //$addressesFrom = $parsedMail->getAllEmailAddresses(array('from'));
+        //$addressesTo = $parsedMail->getAllEmailAddresses(array('to'));
+
+        try {
+            $subject = $parsedMail->getMail()->getHeaderField("Subject");
+        } catch(\Exception $e) {
+            $subject = null;
+        }
+
+        $date = $parsedMail->getMail()->getHeaderField("Date");
+
+        $html2text = new Html2Text($parsedMail->getPrimaryContent());
+        $body = $html2text->get_text();
+
+        try {
+            $activity = $this->am->fromArray(array(
+                'title' => $subject,
+                'executed_at' => $date,
+                'content' => $body,
+            ));
+
+            if(!$dryrun) {
+                $this->em->persist($activity);
+                $this->em->flush($activity);
+            }
+
+            $output->writeln(sprintf("<info>Imported</info>;%s;%s;%s", $date, $subject, str_replace("\n", "", $body)));
+        } catch (\Exception $e) {
+            $output->writeln(sprintf("<error>%s</error>;%s;%s;%s", $e->getMessage(), $date, $subject, str_replace("\n", "", $body)));
+        }
+
+        return true;
+    }
+
+    public function getRootDir() {
+
+        return dirname(__FILE__);
+    }
+
+    public function check($argument) {
+        parent::check($argument);
+
+        if(!file_exists($argument)) {
+            throw new \Exception(sprintf("File %s doesn't exist", $argument));
+        }
+
+        $line = "";
+        $handle = fopen($argument, "r");
+        for($i=1; $i<10; $i++) { $line .= fgets($handle); }
+        fclose($handle);
+
+        if(strpos($line, "Message-ID") === false) {
+           throw new \Exception(sprintf("This file is not a mail file : %s", $argument)); 
+        }
+    }
+
+} 
---
+++ src/AppBundle/Utils/MailParser.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace AppBundle\Utils;
+
+use Html2Text\Html2Text;
+use Zend\Mail\Storage\Part;
+use UnexpectedValueException;
+
+class MailParser extends \Lasso\MailParserBundle\Parser
+{
+}
