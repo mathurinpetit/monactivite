2bbf9153feab67a735056f62e40fc028bf7c5418
Vincent LAURENT <vince.laurent@gmail.com>
2015-06-12 00:59:22 +0200

    Add tag attribute

---
+++ src/AppBundle/Entity/Filter.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Filter
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Filter
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="query", type="string", length=255)
+     */
+    private $query;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Tag")
+     * @ORM\JoinColumn(name="tag_id", referencedColumnName="id")
+     **/
+    private $tag;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set query
+     *
+     * @param string $query
+     * @return Filter
+     */
+    public function setQuery($query)
+    {
+        $this->query = $query;
+
+        return $this;
+    }
+
+    /**
+     * Get query
+     *
+     * @return string 
+     */
+    public function getQuery()
+    {
+        return $this->query;
+    }
+
+    /**
+     * Set tag
+     *
+     * @param \AppBundle\Entity\Tag $tag
+     * @return Filter
+     */
+    public function setTag(\AppBundle\Entity\Tag $tag = null)
+    {
+        $this->tag = $tag;
+
+        return $this;
+    }
+
+    /**
+     * Get tag
+     *
+     * @return \AppBundle\Entity\Tag 
+     */
+    public function getTag()
+    {
+        return $this->tag;
+    }
+}
