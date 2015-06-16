fcf5d4494037ab73bad445cb93bec5bba675ff77
Vincent LAURENT <vince.laurent@gmail.com>
2015-06-17 00:49:33 +0200

    Icon-picker

--- web/js/main.js
+++ web/js/main.js
@@ -3,3 +3,14 @@ $(document).ready( function() {
         theme: 'bootstrap'
     });
 });
+
+$('.iconpicker[data-form-item-relation]').on('change', function(e) {
+    console.log(e.icon);
+    var icon = e.icon.replace("glyphicon-", "");
+    if(icon == "empty") {
+
+        icon = null;
+    }
+    $($(this).attr('data-form-item-relation')).val(icon);
+});
+
